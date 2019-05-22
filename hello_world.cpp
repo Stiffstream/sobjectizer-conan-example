d4f02380490a18f6d0cd531c63109e8f1de0123c
eao197 <eao197@gmail.com>
2019-05-22 10:43:24 +0300

    Adoptation to SO-5.6.0

--- hello_world.cpp
+++ hello_world.cpp
@@ -1,15 +1,19 @@
 #include <so_5/all.hpp>
 
-int main()
-{
+class hello_actor final : public so_5::agent_t {
+public:
+	using so_5::agent_t::agent_t;
+
+	void so_evt_start() override {
+		std::cout << "Hello, World" << std::endl;
+		so_deregister_agent_coop_normally();
+	}
+};
+
+int main() {
+
 	so_5::launch([](so_5::environment_t & env) {
-		env.introduce_coop([](so_5::coop_t & coop) {
-			auto a = coop.define_agent();
-			a.on_start([&coop] {
-				std::cout << "Hello, World" << std::endl;
-				coop.deregister_normally();
-			});
-		});
+		env.register_agent_as_coop(env.make_agent<hello_actor>());
 	});
 
 	return 0;
